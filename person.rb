c815232bed592f263201f8e2195d26512b30c16b
Billions Joel <joelatugonza@gmail.com>
2022-04-05 00:28:46 +0300

    Add constructor

---
+++ person.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
