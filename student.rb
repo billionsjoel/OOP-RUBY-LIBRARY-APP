e36360fa5382fdca39f6088a074e642d21225344
Billions Joel <joelatugonza@gmail.com>
2022-04-05 09:46:23 +0300

    Add super class inheritance and parameters

--- student.rb
+++ student.rb
@@ -1,1 +1,8 @@
 require './person'
+
+class student < Person
+  def initialize(age, clasroom, name='unknown', parent_permission:true)
+  super(age, name, parent_permission)
+  @clasroom = clasroom
+  end
+end
