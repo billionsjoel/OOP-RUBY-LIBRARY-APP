925d3b215915469089e278807bb4b00f6bf4e395
Billions Joel <joelatugonza@gmail.com>
2022-04-05 10:04:46 +0300

    Add super class

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,6 @@
 class Teacher < Person
   def initialize(age, specialization, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 end
